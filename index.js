76183560ee49708d31e30e9e77a81a04beaf54a8
Alesia <jusfanio@gmail.com>
2023-04-20 18:24:42 +0300

    Add title

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+Math.E;
+
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fname) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+sayHelloLinting("James");
+
+console.log(window);
+
+getUser();
